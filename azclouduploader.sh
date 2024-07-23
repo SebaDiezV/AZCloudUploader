18a19f68d63ef2a69a9d9a09742216f2b92a8fd9
SebaDiezV <130402826+SebaDiezV@users.noreply.github.com>
2024-07-23 13:37:08 -0400

    Create azclouduploader.sh

---
+++ azclouduploader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Este script es para subir archivos directamente a a Blob de Azure
+
+#Archivo a subir
+file_path=$1
+
+echo 'Gracias por usar la herramienta AZCloudUploader by SDiezV'
+
+echo 'Versión actual: 1.1'
+
+echo 'Recuerda como paso previo tener instalado Azure CLI en tu dispositivo'
+
+echo 'inicia sesión en Azure para continuar?
+
+#az_login
