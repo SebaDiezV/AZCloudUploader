f137f22044e0bd303a94c392180d0de586b3b186
SebaDiezV <130402826+SebaDiezV@users.noreply.github.com>
2024-07-23 18:07:32 +0000

    save changes

--- azclouduploader.sh
+++ azclouduploader.sh
@@ -6,16 +6,21 @@
 file_path=$1
 
 echo 'Gracias por usar la herramienta AZCloudUploader by SDiezV'
-
+echo
 echo 'Versión actual: 1.1'
-
+echo
 echo 'Recuerda como paso previo tener instalado Azure CLI en tu dispositivo'
-
+echo
 echo 'inicia sesión en Azure para continuar'
 
 #az_login
 
-#prueba
+read -p "Ingresa el nombre de la Cuenta de Almacenamiento:" storage_account_name
+read -p "Ingresa tu Contraseña de almacenamiento:" storage_key
+echo
+echo $storage_account_name
+echo $1
+
 
 
 
