b8b562003b7f42d5b581f791426bbebe8c6df794
SebaDiezV <130402826+SebaDiezV@users.noreply.github.com>
2024-07-23 19:27:10 +0000

    estructura basica

--- azclouduploader.sh
+++ azclouduploader.sh
@@ -13,16 +13,31 @@ echo 'Recuerda como paso previo tener instalado Azure CLI en tu dispositivo'
 echo
 echo 'inicia sesión en Azure para continuar'
 
-#az_login
+##### az login
 
-read -p "Ingresa el nombre de la Cuenta de Almacenamiento:" storage_account_name
-read -p "Ingresa tu Contraseña de almacenamiento:" storage_key
 echo
-echo "Revisando Cuenta de Almacenamiento $storage_account_name
+#información de cuenta de almacenamiento
+read -p "Ingresa el nombre de la Cuenta de Almacenamiento: " storage_account_name
+read -s -p "Ingresa tu Contraseña de almacenamiento: " storage_key
+echo
+echo "Revisando Cuenta de Almacenamiento" $storage_account_name
+
+###script para mostrar los containers
+
 echo 
 
+# mostrar containers en cuenta de almacenamiento
+echo "estos son los containers disponibles en la cuenta de almacenamiento"
 
+#######escript para containers
+echo
+read -p "Ingresa el nombre del container al que deseas subir el archivo: " container_name
 
+echo "subiendo archivo..."
 
-
-
+# Subir Azure Blob Storage
+az storage blob upload \
+  --account-name $storage_account_name \
+  --account-key $storage_account_key \
+  --container-name $container_name \
+  --file $file_path
